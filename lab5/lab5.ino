f9ce7e1cd8ce91b2d52c3a44b6ea07a51ba1c06d
Emma Raymond <ecraymond1@gmail.com>
2026-03-26 11:17:57 -0400

    Troubleshooting

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -30,7 +30,6 @@ volatile long count_right = 0;
 // ============================
 float rotation = 3.125;
 int pwm = 0;
-int count_PWM[255];
 
 // sample time in ms
 const unsigned long sampleTime = 100;
@@ -116,26 +115,6 @@ void setup() {
 // Loop
 // ============================
 void loop() {
-
-  noInterrupts();
-  long leftCount = count_left;
-  long rightCount = count_right;
-  count_left = 0;
-  count_right = 0;
-  interrupts();
-
-  delay(100);
-
-  float rpmLeft = leftCount * rotation;
-  float rpmRight = rightCount * rotation;
-
-  Serial.print("PWM: ");
-  Serial.print(count_PWM[count_left]);
-  Serial.print("   Left RPM: ");
-  Serial.print(rpmLeft);
-  Serial.print("   Right RPM: ");
-  Serial.println(rpmRight);
-
   if (Serial2.available()) {
     char cmd = Serial2.read();
 
@@ -153,13 +132,11 @@ void loop() {
       for(int i = 0; i < 255; i += 5){
         pwm = i;
         Forward(i);
-        //delay(100);        
-        count_PWM[i] = pwm;
       }
     }
   }
 
-  /*noInterrupts();
+  noInterrupts();
   long leftCount = count_left;
   long rightCount = count_right;
   count_left = 0;
@@ -172,11 +149,11 @@ void loop() {
   float rpmRight = rightCount * rotation;
 
   Serial.print("PWM: ");
-  Serial.print(count_PWM[count_left % 5]);
+  Serial.print(pwm);
   Serial.print("   Left RPM: ");
   Serial.print(rpmLeft);
   Serial.print("   Right RPM: ");
-  Serial.println(rpmRight);*/
+  Serial.println(rpmRight);
 }
 
 //     if (Serial2.available()){ //send to car
